283722f1747566e538586a2376d6be79becb369c
wdcdz <812150635@qq.com>
2021-11-13 16:33:31 +0800

    commonjs模块化

---
+++ 02_CommonJS-Node/app.js
@@ -0,0 +1,8 @@
+let module1 = require('./modules/module1')
+let module2 = require('./modules/module2')
+let module3 = require('./modules/module3')
+
+console.log(module1);
+module2()
+module3.foo()
+module3.bar()
